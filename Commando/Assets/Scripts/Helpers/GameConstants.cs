13085348ab292e2f26056b9585185b21abc17327
101011 <dayxx446@umn.edu>
2018-11-27 10:39:59 -0600

    Add meta and material files

---
+++ Commando/Assets/Scripts/Helpers/GameConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Helpers {
+    public static class GameConstants
+    {
+        public static int TileSize = 72;  // the size of a background tile in pixels
+
+    }
+}
---
+++ Commando/Assets/Scripts/Helpers/GameVariables.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Helpers {
+    public static class GameVariables
+    {
+
+        public static int XRes = 2560;
+        public static int YRes = 1440;
+
+        public static float ActorRotateSpeed = 45;
+        public static float ActorMoveSpeed = 2;
+        public static double ActorReach = 0.3;
+
+        public static double Tolerance = 0.05;
+    }
+}
